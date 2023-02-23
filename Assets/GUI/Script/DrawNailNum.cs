0a9f2e1f67f895d7d118e6305e5a52f6f372919d
ninomiyaren <ninomiyaren1013@gmail.com>
2023-02-23 22:39:16 +0900

    釘の概念追加、釘の所持数をUIに表示、釘拾う処理追加

---
+++ Assets/GUI/Script/DrawNailNum.cs
@@ -0,0 +1,51 @@
+//---------------------------------------
+//担当者：二宮
+//内容　：釘所持数をUIとして表示
+//---------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawNailNum : MonoBehaviour
+{
+    //---------------------------------------------------------
+    // - 変数宣言 -
+
+    private Text TextNailNum; // ゲーム画面に描画する
+    public int oldNailsNum; // 前フレームの釘の数を保存する
+
+    // 外部取得
+    private GameObject player; // プレイヤーを見つけて保持する
+    private HaveNails nails; // HaveNailsを保持
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //---------------------------------------------------------
+        // プレイヤー見つける
+        player = GameObject.Find("player");
+
+        // HaveNailsを取得
+        nails = player.GetComponent<HaveNails>();
+
+        TextNailNum = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 前フレームの釘の数から変動があれば
+        if(oldNailsNum != nails.NailsNum)
+        {
+            
+        }
+
+        //---------------------------------------------------------
+        // プレイヤーが所持する釘の数を再描画
+        TextNailNum.text = string.Format("×{0:00}", nails.NailsNum);
+
+        // 古い釘更新
+        oldNailsNum = nails.NailsNum;
+    }
+}
---
+++ Assets/Player/Script/HaveNails.cs
@@ -0,0 +1,33 @@
+//---------------------------------------
+//担当者：二宮
+//内容　：プレイヤーが持つ釘の数を管理
+//---------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaveNails : MonoBehaviour
+{
+    //---------------------------------------------------------
+    // - 変数宣言 -
+    private string NailTag = "Nail"; //タグ名
+
+    [Header("釘所持数")]
+    public int NailsNum = 0; // 持っている釘の数
+
+    //落ちている釘に触れると釘所持数が増える
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //---------------------------------------------------------
+        // 触れたのがアイテムとしての釘なら釘所持数増える
+
+        // タグが釘なら
+        if (collision.tag == NailTag)
+        {
+            NailsNum++;
+        }
+
+        // アイテムとしての釘は消す
+        Destroy(collision.gameObject);
+    }
+}
