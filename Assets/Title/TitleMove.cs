46d9a356f7f995fea291383bcaa42550596c9688
WIN11OHS10930\尾花　真理子 <kazimari-0609@docomo.ne.jp>
2023-03-15 18:58:37 +0900

    タイトルシーンでヒビが入る演出実装しました。 Aボタンを押す度にヒビが入ります。

--- Assets/Title/TitleMove.cs
+++ Assets/Title/TitleMove.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class TitleMove : MonoBehaviour
 {
-   
+    public int FallNum; // タイトル画面が崩壊するまでの数
+
+    string TitleBackName;   //　背景のオブジェクト名
+
+    SpriteRenderer MainSpriteRenderer;
+
+    public List<Sprite> TitleBackSprite;
 
     // Start is called before the first frame update
     void Start()
     {
+        //タイトルのSptiteRenderereを取得
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
     }
 
@@ -19,8 +27,23 @@ public class TitleMove : MonoBehaviour
         //  キー入力
         if (Input.GetKeyDown("joystick button 0"))
         {
-            
-            SceneManager.LoadScene("SelectScene");
+            FallNum--;
+
+            //-------------------------------------------------
+            //スプライトを変更
+            if (FallNum > 0)
+            {
+                MainSpriteRenderer.sprite = TitleBackSprite[FallNum - 1];
+            }
+            //-------------------------------------------------
+
+            //---------------------------------------------------
+            // 崩壊までのカウントダウンが0になったらシーン移動
+            if (FallNum == 0)
+            {
+                SceneManager.LoadScene("SelectScene");
+            }
+
         }
 
     }
