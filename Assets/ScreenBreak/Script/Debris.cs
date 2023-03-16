6b8590f76c89bca338471566bacda297ee584798
WIN11OHS10817\kosuk <kosuke1229gou@gmail.com>
2023-03-17 04:42:03 +0900

    2023-03-17 画面が割れる演出（仮案）の実装

---
+++ Assets/ScreenBreak/Script/Debris.cs
@@ -0,0 +1,81 @@
+//------------------------------------------------------------------------------
+// 担当：藤原昂祐
+// 内容：破片
+//------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debris : MonoBehaviour
+{
+    //--------------------------------------------------------------------------
+    // - 変数宣言 -
+
+    // 座標関連
+    Vector3 startRot;// 生成時の回転率
+    Vector3 startPos;// 生成時の座標
+
+    //============================================================
+    // - 初期化処理 -
+
+    void Start()
+    {
+        //-------------------------------------------------------
+        // 初期位置を保存する
+
+        Transform objTransform = this.transform;
+        startRot = objTransform.eulerAngles;
+        startPos = objTransform.position;
+    }
+
+    //============================================================
+    // - 更新処理 -
+
+    void Update()
+    {
+        //--------------------------------------------------------
+        // 落下時の挙動を制御する
+
+        // 回転させる
+        startRot.z += 0.1f;
+
+        // このオブジェクトのTransformを取得
+        Transform objTransform = this.transform;
+
+        // 座標の指定
+        Vector3 pos = objTransform.position;
+        // 真ん中より右なら右に移動する
+        if(pos.x > 2.0f)
+        {
+            pos.x += 0.01f;
+        }
+        // 真ん中より左なら左に移動する
+        if (pos.x < -2.0f)
+        {
+            pos.x -= 0.01f;
+        }
+        // 最初は少し上に移動させる
+        if (startPos.y > pos.y - 2.0f)
+        {
+            pos.y += 0.01f;
+        }
+       
+        // 回転率の指定
+        Vector3 rot;
+        rot.x = 1.0f;
+        rot.y = 1.0f;
+        rot.z = startRot.z;
+
+        // 座標、回転率を敵用する
+        objTransform.eulerAngles = rot;
+        objTransform.position = pos;
+
+        // 画面外ならこのオブジェクトを消去する
+        if (pos.y < -8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    //============================================================
+
+}
---
+++ Assets/ScreenBreak/Script/ScreenBreak.cs
@@ -0,0 +1,87 @@
+//------------------------------------------------------------------------------
+// 担当：藤原昂祐
+// 内容：画面が割れた演出
+//------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBreak : MonoBehaviour
+{
+    //--------------------------------------------------------------------------
+    // - 変数宣言 -
+
+    // 破片関連
+    [Header("破片のオブジェクト")]
+    public GameObject debris;// 破片用オブジェクト
+    [Header("破片の生成数")]
+    public int amount = 50;  // 破片の生成数
+
+    // 音関連
+    [Header("効果音")]
+    public AudioClip sound1;// 音声ファイル
+    AudioSource audioSource;// 取得したAudioSourceコンポーネント
+
+    //============================================================
+    // - 初期化処理 -
+
+    void Start()
+    {
+        // AudioSourceコンポーネントを取得
+        audioSource = GetComponent<AudioSource>();
+
+        Break();
+    }
+
+    //============================================================
+    // - 画面を割る演習をする処理 -
+    
+    void Break()
+    {
+        //--------------------------------------------------------
+        // 音声ファイルを再生する
+        audioSource.PlayOneShot(sound1);
+
+        //--------------------------------------------------------
+        // 破片を生成する
+
+        for (int i = 0; i < amount; i++)
+        {
+            // ランダムに座標、大きさ、回転率を取得する
+            int rndX = Random.Range(1, 20);
+            int rndY = Random.Range(1, 20);
+            int rndSizeX = Random.Range(1, 10);
+            int rndSizeY = Random.Range(1, 10);
+            int rndRot = Random.Range(1, 360);
+
+            // 破片を生成
+            GameObject obj = Instantiate(debris, transform.position, Quaternion.identity);
+
+            Transform objTransform = obj.transform;
+
+            // 座標を変更
+            Vector3 pos = objTransform.position;
+            pos.x = -8.0f + 0.8f * rndX;
+            pos.y = -8.0f + 0.8f * rndY;
+            pos.z = 0.0f;
+
+            // 大きさを変更
+            Vector3 scale;
+            scale.x = 1.0f * rndSizeX;
+            scale.y = 1.0f * rndSizeY;
+            scale.z = 1.0f;
+
+            // 回転を変更
+            Vector3 rot;
+            rot.x = 1.0f;
+            rot.y = 1.0f;
+            rot.z = 1.0f * rndRot;
+
+            // 変更を敵用する
+            objTransform.position = pos;    // 座標
+            objTransform.localScale = scale;// 大きさ
+            objTransform.eulerAngles = rot; // 回転
+        }
+    }
+    //============================================================
+}
