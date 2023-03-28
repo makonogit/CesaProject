48c04ade3190c40c681f93c7a9f37bc7a29addc0
WIN11OHS10817\kosuk <kosuke1229gou@gmail.com>
2023-03-29 04:35:22 +0900

    クリア演出（リザルト）の追加

---
+++ Assets/Effect/Clear Effect/Script/ClearDebris.cs
@@ -0,0 +1,132 @@
+//------------------------------------------------------------------------------
+// 担当：藤原昂祐
+// 内容：クリア演出用の破片
+//------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearDebris : MonoBehaviour
+{
+    //--------------------------------------------------------------------------
+    // - 変数宣言 -
+
+    // 座標関連
+    Vector3 startRot;       // 生成時の回転率
+    Vector3 startPos;       // 生成時の座標
+    public Vector3 clearPos;// テキスト生成時の座標
+
+    // テキスト関連
+    bool isMoveFlg = false;// 移動開始フラグ
+    int delayCnt = 0;      // 待機時間をカウント
+
+    //============================================================
+    // - 初期化処理 -
+
+    void Start()
+    {
+        //-------------------------------------------------------
+        // 初期位置を保存する
+
+        Transform objTransform = this.transform;
+        startRot = objTransform.eulerAngles;
+        startPos = objTransform.position;
+    }
+
+    //============================================================
+    // - 更新処理 -
+
+    void Update()
+    {
+        //--------------------------------------------------------
+        // 落下時の挙動を制御する
+
+        // このオブジェクトのTransformを取得
+        Transform objTransform = this.transform;
+
+        // 座標の指定
+        Vector3 pos = objTransform.position;
+
+        if (isMoveFlg == false)
+        {
+            // 落下させる
+            if (pos.y > -5.0f)
+            {
+                pos.y -= 0.02f;
+            }
+
+            // 回転させる
+            if (pos.y > -4.0f)
+            {
+                startRot.z += 0.1f;
+            }
+
+            // 画面の下まで落ちたら待機
+            if(pos.y < -5.0f)
+            {
+                delayCnt++;
+               
+            }
+
+            // 待機時間が終わったら移動開始
+           if(delayCnt > 60 * 8)
+            {
+                isMoveFlg = true;
+                delayCnt = 0;
+            }
+        }
+        else
+        {
+
+            //--------------------------------------------------------
+            // テキスト生成時の挙動を制御する
+
+            if (clearPos.z != 0.0f)
+            {
+               
+                // 大きさをテキスト用に小さくする
+                Vector3 scale = objTransform.localScale;
+                scale.x = 0.8f;
+                scale.y = 0.8f;
+                scale.z = 1.0f;
+                objTransform.localScale = scale;
+
+                // テキストの位置まで移動する
+                if (clearPos.x > pos.x)
+                {
+                    pos.x += 0.01f;
+                    
+                }
+                if (clearPos.x < pos.x)
+                {
+                    pos.x -= 0.01f;
+                    
+                }
+                if (clearPos.y > pos.y)
+                {
+                    pos.y += 0.01f;
+                    startRot.z += 0.1f;
+                }
+            }
+
+           
+        }
+
+        //--------------------------------------------------------
+        // Transformを敵用する
+
+        // 回転率を敵用する
+        Vector3 rot;
+        rot.x = 1.0f;
+        rot.y = 1.0f;
+        rot.z = startRot.z;
+        objTransform.eulerAngles = rot;
+
+        // 座標を敵用する
+        objTransform.position = pos;
+
+    }
+    //============================================================
+}
+
+
---
+++ Assets/Effect/Clear Effect/Script/ClearEffect.cs
@@ -0,0 +1,154 @@
+//------------------------------------------------------------------------------
+// 担当：藤原昂祐
+// 内容：クリア演出
+//------------------------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearEffect : MonoBehaviour
+{
+    //--------------------------------------------------------------------------
+    // - 変数宣言 -
+
+    bool isFadeFlg = false;// 画面を割る処理のON、OFF
+
+    // フェード関連
+    [Header("フェードアウトする速度")]
+    public float fadeSpeed = 0.001f;// フェードアウトする速度
+    float alpha = 0.0f;             // パネルの透明度
+
+    // 破片関連
+    [Header("破片のオブジェクト")]
+    public GameObject debris;// 破片用オブジェクト
+    [Header("破片の生成数")]
+    public int amount = 95;  // 破片の生成数
+    GameObject[] debrisRist = new GameObject[95];
+
+    // 音関連
+    [Header("効果音")]
+    public AudioClip sound1;// 音声ファイル
+    AudioSource audioSource;// 取得したAudioSourceコンポーネント
+
+    // クリアテキスト関連
+    int[] clear = new int[]{
+
+          1,1,1,0,1,0,0,0,1,1,1,0,0,1,0,0,1,1,0 ,
+          1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,1 ,
+          1,0,0,0,1,0,0,0,1,1,1,0,1,1,1,0,1,1,1 ,
+          1,0,0,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,1 ,
+          1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0,1 ,
+    };
+
+    //============================================================
+    // - 初期化処理 -
+
+    void Start()
+    {
+        // AudioSourceコンポーネントを取得
+        audioSource = GetComponent<AudioSource>();
+
+        SetFadeFlg(true);
+    }
+
+    //============================================================
+    // - 更新処理 -
+
+    void Update()
+    {
+        //--------------------------------------------------------
+        // 画面が見えなくなるまでフェードアウト後、破片を生成する
+
+        if (isFadeFlg == true)
+        {
+            if (alpha >= 1.0f)
+            {
+                //--------------------------------------------------------
+                // 効果音を再生
+
+                audioSource.PlayOneShot(sound1);
+
+                //--------------------------------------------------------
+                // 破片を生成
+
+                for (int i = 0; i < amount; i++)
+                {
+
+                    // ランダムに座標、大きさ、回転率を取得する
+                    int rndX = Random.Range(0, 21);
+                    int rndY = Random.Range(0, 21);
+                    int rndSizeX = Random.Range(1, 10);
+                    int rndSizeY = Random.Range(1, 10);
+                    int rndRot = Random.Range(1, 360);
+
+                    // 破片を生成
+                    debrisRist[i] = Instantiate(debris, transform.position, Quaternion.identity);
+
+                    // 破片のクリア後の座標を決定
+                    if (clear[i] == 1)
+                    {
+                        ClearDebris clearDebris = debrisRist[i].GetComponent<ClearDebris>();
+                        clearDebris.clearPos.x = -7.0f + 0.8f * (i % 19);
+                        clearDebris.clearPos.y = 4.0f - 0.8f * (i / 19);
+                        clearDebris.clearPos.z = 2.0f;
+                    }
+
+                    Transform objTransform = debrisRist[i].transform;
+
+                    // 座標を変更
+                    Vector3 pos = objTransform.position;
+                    pos.x = -8.0f + 0.8f * rndX;
+                    pos.y = -8.0f + 0.8f * rndY;
+                    pos.z = 0.0f;
+
+                    // 大きさを変更
+                    Vector3 scale;
+                    scale.x = 0.5f * rndSizeX;
+                    scale.y = 0.5f * rndSizeY;
+                    scale.z = 1.0f;
+
+                    // 回転を変更
+                    Vector3 rot;
+                    rot.x = 1.0f;
+                    rot.y = 1.0f;
+                    rot.z = 1.0f * rndRot;
+
+                    // 変更を敵用する
+                    objTransform.position = pos;    // 座標
+                    objTransform.localScale = scale;// 大きさ
+                    objTransform.eulerAngles = rot; // 回転
+
+                }
+
+                //--------------------------------------------------------
+                // フェード処理をOFFにする
+
+                isFadeFlg = false;
+                alpha = 0.0f;
+                
+            }
+            else
+            {
+
+                //--------------------------------------------------------
+                // 画面が見えなくなるまでフェードアウトする
+
+                // 透明度を加算
+                alpha += fadeSpeed;
+            }
+
+            // alphaの値を敵用する
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, alpha);
+        }
+    }
+
+    //============================================================
+    // - isScreenBreakFlg用セッター -
+
+    public void SetFadeFlg(bool _isSet)
+    {
+        isFadeFlg = _isSet;
+    }
+    //============================================================
+}
