a0b612d022a1bb043b15051b2017031f13ca4c49
mako_tok <makosan5103@gmail.com>
2023-04-30 20:15:30 +0900

    エリア2，3のBGM再生

--- Assets/Stage/Sound/PlayBgm.cs
+++ Assets/Stage/Sound/PlayBgm.cs
@@ -14,6 +14,11 @@ public class PlayBgm : MonoBehaviour
     AudioSource Intro;  // イントロ
     AudioSource Loop;   // ループ
 
+   [SerializeField, Header("各エリアイントロBGM")]
+    private List<AudioClip> Loop_Bgm;
+
+    private SetStage Stage;
+
     // 二宮追加
     [Header("ループに切り替わる時間")]
     public float LoopStartTime;
@@ -26,18 +31,20 @@ public class PlayBgm : MonoBehaviour
         // AudioSourceを取得
         Intro = transform.GetChild(0).GetComponent<AudioSource>();
         Loop = transform.GetChild(1).GetComponent<AudioSource>();
-      
 
+        Stage = new SetStage();
+        if (Stage.GetAreaNum() != 0) LoopStartTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // Debug.Log(Intro.time);
+        Debug.Log(Intro.time);
 
         // イントロが再生が終了したら
         if (Intro.time > LoopStartTime && Intro.isPlaying)
         {
+            Loop.clip = Loop_Bgm[Stage.GetAreaNum()];
             Debug.Log("loop");
             Intro.Stop();
             Loop.Play();
---
+++ Assets/Stage/Sound/PlayIntroBGM.cs
@@ -0,0 +1,22 @@
+//----------------------------
+//　担当：菅眞心
+//　内容：エリアごとのBGM
+//----------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayIntroBGM : MonoBehaviour
+{
+    [SerializeField, Header("ステージごとのIntroBGM")]
+    private List<AudioClip> Intro_Bgm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetStage stage = new SetStage();
+        GetComponent<AudioSource>().clip = Intro_Bgm[stage.GetAreaNum()];
+        GetComponent<AudioSource>().Play();
+
+    }
+}
