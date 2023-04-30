870593389e415abc48e36ffd8340df699636c90e
mako_tok <makosan5103@gmail.com>
2023-04-30 17:31:50 +0900

    nullチェックのところでエラー出るので更新

--- Assets/Game/Enemy/TownEnemy/Script/EnemyCrackHit.cs
+++ Assets/Game/Enemy/TownEnemy/Script/EnemyCrackHit.cs
@@ -40,10 +40,13 @@ public class EnemyCrackHit : MonoBehaviour
                 order = collision.gameObject.GetComponent<CrackCreater>();
 
                 //ê∂ê¨íÜÇ»ÇÁ
-                if (order != null && order.State == CrackCreater.CrackCreaterState.CREATING || order.State == CrackCreater.CrackCreaterState.ADD_CREATING)
+                if (order != null)
                 {
-                    // éÄñSèÛë‘Ç…Ç∑ÇÈ
-                    enemyMove.EnemyAI = EnemyMove.AIState.DEATH;
+                    if (order.State == CrackCreater.CrackCreaterState.CREATING || order.State == CrackCreater.CrackCreaterState.ADD_CREATING)
+                    {
+                        // éÄñSèÛë‘Ç…Ç∑ÇÈ
+                        enemyMove.EnemyAI = EnemyMove.AIState.DEATH;
+                    }
                 }
             }
         }
