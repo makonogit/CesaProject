6193f92c29a0eabef9ef89613c80809214cf4183
WIN10OHS10596\utt01 <utt014257u6s02@outlook.jp>
2023-05-02 22:31:34 +0900

    ぼすBGMの更新忘れ

--- Assets/Stage/Stagegimmick/Gate/Script/GateThrough.cs
+++ Assets/Stage/Stagegimmick/Gate/Script/GateThrough.cs
@@ -14,6 +14,18 @@ public class GateThrough : MonoBehaviour
     [SerializeField, Header("ブロックのTransform")]
     private Transform Blocktransform;
 
+    [SerializeReference, Header("BGM用AudioSorce")]
+    private AudioSource BGM;
+
+    [SerializeReference, Header("ボスBGM用AudioSorce")]
+    private AudioSource BossBGM;
+
+    private void Start()
+    {
+        BGM = GameObject.Find("BGM(Loop)").GetComponent<AudioSource>();
+        BossBGM = GameObject.Find("BossBGM").GetComponent<AudioSource>();
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         // プレイヤーがすり抜けたら生成
@@ -21,6 +33,8 @@ public class GateThrough : MonoBehaviour
         {
             Destroy(GetComponent<BoxCollider2D>());
             GameObject obj = Instantiate(GateBlock, Blocktransform);
+            BGM.Stop();
+            BossBGM.Play();
             obj.transform.parent = null;
         }
     }
