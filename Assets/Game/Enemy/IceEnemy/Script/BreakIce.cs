9ab0b7a9325deb229cef1cb909997953b4b4fab6
WIN10OHS10596\utt01 <utt014257u6s02@outlook.jp>
2023-05-11 19:58:58 +0900

    エリア2の敵が氷を壊すようになりました。

---
+++ Assets/Game/Enemy/IceEnemy/Script/BreakIce.cs
@@ -0,0 +1,18 @@
+﻿//----------------------------------------------------------
+// 担当者：中川直登
+// 内容  ：プレイヤーが範囲に入ったかどうか
+//----------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BreakIce : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ice") 
+        {
+            BreakBlock Ice = collision.GetComponent<BreakBlock>();
+            if (Ice != null) Ice.Func_BreakBlock();
+        }
+    }
+}
