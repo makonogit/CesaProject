72f8bae44d8549ef34838c2bc44d1dfc28c4d5cf
mako_tok <makosan5103@gmail.com>
2023-05-20 18:52:21 +0900

    砂のギミック修正

--- Assets/Stage/Stagegimmick/Sand/Script/Sand.cs
+++ Assets/Stage/Stagegimmick/Sand/Script/Sand.cs
@@ -12,8 +12,12 @@ public class Sand : MonoBehaviour
     //　変数宣言
     private SpriteRenderer thismat;  //このオブジェクトのRender
     public bool SandHit = false;   //砂が当たっているか   
-    private float Line = 0.0f;
+    [SerializeField]private float Line = 0.0f;
+    
     private PolygonCollider2D thiscoll; //このオブジェクトのcollider
+    private float MaxSand;
+
+    private float Wait = 0.0f; //時間計測用
 
     [SerializeField, Header("つもるスピード")]
     private float accumulatespeed;
@@ -24,23 +28,27 @@ public class Sand : MonoBehaviour
     {
         thismat = GetComponent<SpriteRenderer>();
         thiscoll = GetComponent<PolygonCollider2D>();
-
+        MaxSand = thiscoll.points[0].y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //SandHit = false;
         //砂が当たっていたら徐々に見えるようにする
         if (SandHit)
         {
-            if (Line <= 1.0f)
+            if (Line < 1.0f)
             {
                 Line += accumulatespeed * Time.deltaTime;
-                //Vector2[] points = thiscoll.points;
-                //points[0].y += Line;
-                //points[1].y += Line;
-                //thiscoll.SetPath(0, points);
+                Vector2[] points = thiscoll.points;
+                
+                if (points[0].y < MaxSand)
+                {
+                    points[0].y += Line;
+                    points[1].y += Line;
+                }
+
+                thiscoll.SetPath(0, points);
             }
             else
             {
@@ -53,15 +61,35 @@ public class Sand : MonoBehaviour
             if (Line > 0.0f)
             {
                 Line -= accumulatespeed * Time.deltaTime;
-                //Vector2[] points = thiscoll.points;
-                //points[0].y -= Line;
-                //points[1].y -= Line;
-                //thiscoll.SetPath(0, points);
+                Vector2[] points = thiscoll.points;
+                if (points[0].y > points[2].y)
+                {
+                    points[0].y -= Line;
+                    points[1].y -= Line;
+                }
+                thiscoll.SetPath(0, points);
 
+            }
+            else
+            {
+                gameObject.layer = 15;
             }
         }
 
-        
+        // 0.5秒待って当たり判定を無効に
+        if (SandHit)
+        {
+            Wait += Time.deltaTime;
+            if (Wait > 0.3f)
+            {
+                SandHit = false;
+                Wait = 0.0f;
+            }
+        }
+        else
+        {
+            SandHit = false;
+        }
 
         thismat.material.SetFloat("_Border", Line);
     }
@@ -69,7 +97,7 @@ public class Sand : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //砂が当たっていたら
-        if (collision.gameObject.tag == "Sand")
+        if (collision.gameObject.tag == "Sand" && !SandHit)
         {
             SandHit = true;
         }
@@ -80,6 +108,7 @@ public class Sand : MonoBehaviour
         //砂が当たらなくなったら
         if (collision.gameObject.tag == "Sand")
         {
+            Debug.Log("当たらなかった");
             SandHit = false;
         }
     }
